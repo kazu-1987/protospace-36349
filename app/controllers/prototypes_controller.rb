5fb5edb928b6af90377bb636c33df862f78a116a
kazu-1987 <otsuji.k1123@gmail.com>
2021-09-19 08:52:57 +0900

    Revert "second commit"
    This reverts commit c7bc4006dc7322f20a9d1ce3dc4733e70c4a9a8d.

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -57,7 +57,8 @@ class PrototypesController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in?
+    prototype = Prototype.find(params[:id])
+    if prototype.user_id != current_user.id
       redirect_to action: :index
     end
   end
